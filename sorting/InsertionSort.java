36d5a668bee9ff39d290358bd551f35b419d2eff
Mariano <mariano_tangari@hotamil.com>
2023-04-07 19:05:07 -0300

    Added InsertionSort.java

---
+++ sorting/InsertionSort.java
@@ -0,0 +1,29 @@
+import java.util.Arrays; 
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class InsertionSort {
+	
+	public static void main(String[] args) {
+		
+		int[] arr = IntStream.generate(() -> new Random().nextInt(20)).limit(30).toArray();
+		
+		insertionSort(arr); 
+	}
+	
+	private static void insertionSort(int[] arr) {
+		
+		for (int i = 1; i < arr.length; i++) {
+			
+			int key = arr[i]; 
+			int j = i - 1; 
+			
+			while (j >= 0 && arr[j] > key) 
+				arr[j + 1] = arr[j--]; 
+			
+			arr[++j] = key; 
+		}
+	}
+}
+			
+		

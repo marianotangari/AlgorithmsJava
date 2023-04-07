db360ff2436e07c34c21ba579387da7f45a12b3d
Mariano <mariano_tangari@hotamil.com>
2023-04-07 19:09:04 -0300

    Compressed code.

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -13,13 +13,10 @@ public class InsertionSort {
 	
 	private static void insertionSort(int[] arr) {
 		for (int i = 1; i < arr.length; i++) {
-			
 			int key = arr[i]; 
 			int j = i - 1; 
-			
 			while (j >= 0 && arr[j] > key) 
 				arr[j + 1] = arr[j--]; 
-			
 			arr[++j] = key; 
 		}
 	}

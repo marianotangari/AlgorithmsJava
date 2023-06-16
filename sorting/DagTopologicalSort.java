92986b050ed81dd56f7131134a41b35623997272
Mariano Tangari <50705763+marianotangari@users.noreply.github.com>
2023-06-15 21:15:19 -0300

    Update DagTopologicalSort.java

--- sorting/DagTopologicalSort.java
+++ sorting/DagTopologicalSort.java
@@ -20,7 +20,7 @@ public class DagTopologicalSort {
 
         Digraph<String> graph = new Digraph<>();
 
-		LinkedList<Vertex<String>> listA = new LinkedList<>();
+	LinkedList<Vertex<String>> listA = new LinkedList<>();
         LinkedList<Vertex<String>> listB = new LinkedList<>();
         LinkedList<Vertex<String>> listC = new LinkedList<>();
         LinkedList<Vertex<String>> listD = new LinkedList<>();
@@ -28,20 +28,20 @@ public class DagTopologicalSort {
         LinkedList<Vertex<String>> listF = new LinkedList<>();
         LinkedList<Vertex<String>> listG = new LinkedList<>();
 		
-		listA.offer(e); 
-		listA.offer(c);
-		listA.offer(b); 
+	listA.offer(e); 
+	listA.offer(c);
+	listA.offer(b); 
 		
-		listC.offer(e);
-		listC.offer(d);
+	listC.offer(e);
+	listC.offer(d);
 		
-		listE.offer(f);
+	listE.offer(f);
 		
-		listG.offer(c); 
+	listG.offer(c); 
 		
-		Digraph<String> digraph = new Digraph<>(); 
+	Digraph<String> digraph = new Digraph<>(); 
 		
-		graph.addVertexList(a, listA);
+	graph.addVertexList(a, listA);
         graph.addVertexList(b, listB);
         graph.addVertexList(c, listC);
         graph.addVertexList(d, listD);
@@ -92,42 +92,42 @@ public class DagTopologicalSort {
  class Digraph<T> {
 
     Map<Vertex<T>, LinkedList<Vertex<T>>> adjList;
-	List<Vertex<T>> vertices;
+    private List<Vertex<T>> vertices;
+    
+    Digraph() {
+	this.adjList = new HashMap<>();
+    }
 
-	Digraph() {
-		this.adjList = new HashMap<>();
-	}
+    void addVertexList(Vertex<T> vertex, LinkedList<Vertex<T>> list) {
+	adjList.put(vertex, list);
+    }
 
-	void addVertexList(Vertex<T> vertex, LinkedList<Vertex<T>> list) {
-		adjList.put(vertex, list);
-	}
+    LinkedList<Vertex<T>> getVertexList(Vertex<T> vertex) {
+	return adjList.get(vertex);
+    }
 
-	LinkedList<Vertex<T>> getVertexList(Vertex<T> vertex) {
-		return adjList.get(vertex);
-	}
+    Map<Vertex<T>, LinkedList<Vertex<T>>> getAdjList() {
+	return adjList;
+    }
 
-	Map<Vertex<T>, LinkedList<Vertex<T>>> getAdjList() {
-		return adjList;
-	}
+    void setVertices(Vertex<T>... vertices) {
+	this.vertices = Arrays.asList(vertices);
+    }
 
-	void setVertices(Vertex<T>... vertices) {
-		this.vertices = Arrays.asList(vertices);
-	}
-
-	List<Vertex<T>> getVertices() {
-		return vertices;
-	}
+    List<Vertex<T>> getVertices() {
+	return vertices;
+    }
 }
 
- class Vertex<T> {
+class Vertex<T> {
 
     static enum Color { WHITE, GRAY, BLACK }
 
-	Color color;
-	int d;
-	int f;
-	Vertex<T> prev;
-	String value;
+    private Color color;
+    private int d;
+    private int f;
+    private Vertex<T> prev;
+    private String value;
 
     public Vertex(Color color, String value) {
         this.color = color;

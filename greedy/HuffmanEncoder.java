e08204ea982b330d5fe0591d0a014476d8e9aaa0
Mariano <mariano_tangari@hotamil.com>
2023-07-10 23:36:42 -0300

    HufmannEncoder.java - first commit.

---
+++ greedy/HuffmanEncoder.java
@@ -0,0 +1,111 @@
+import java.util.*;
+
+public class HuffmanEncoder {
+
+        public static void main(String[] args) {
+            encode(args[0]);
+        }
+
+        private static void encode(String str) {
+            HuffmanNode tree = buildHuffmanTree(buildNodeMap(str));
+            tree.printBfs();
+        }
+
+        private static Map<Character, HuffmanNode> buildNodeMap(String str) {
+
+            Map<Character, HuffmanNode> map = new HashMap<>();
+
+            str.chars().forEach( letter -> {
+                char c = (char) letter;
+                map.putIfAbsent(c, new HuffmanLeaf(c, 0));
+                map.computeIfPresent(c, (w, node) -> node.increaseFreq());
+            });
+            return map;
+        }
+
+        private static HuffmanNode buildHuffmanTree(Map<Character, HuffmanNode> map) {
+
+            PriorityQueue<HuffmanNode> queue = new PriorityQueue<HuffmanNode>();
+            map.values().forEach(v -> queue.offer(v));
+
+            for (int i = 0; i < map.size() - 1; i++) {
+                HuffmanNode left = queue.poll();
+                HuffmanNode right = queue.poll();
+                HuffmanNode node = new HuffmanNode(left, right, left.getFreq() + right.getFreq());
+                queue.offer(node);
+            }
+            return queue.poll();
+        }
+}
+
+class HuffmanNode implements Comparable<HuffmanNode>{
+
+    private HuffmanNode left;
+    private HuffmanNode right;
+    protected long freq;
+
+    public HuffmanNode(HuffmanNode left, HuffmanNode right, long freq) {
+        this.left = left;
+        this.right = right;
+        this.freq = freq;
+    }
+    public HuffmanNode(long freq) {
+        this.freq = freq;
+    }
+    public HuffmanNode getLeft() {
+        return left;
+    }
+    public HuffmanNode getRight() {
+        return right;
+    }
+    public long getFreq() {
+        return freq;
+    }
+    public HuffmanNode increaseFreq(){
+        freq++;
+        return this;
+    }
+    @Override
+    public String toString() {
+        return String.format("Node freq: %d", this.getFreq());
+    }
+    @Override
+    public int compareTo(HuffmanNode o) {
+        if (o.getFreq() >= this.getFreq()) {
+            return -1;
+        }
+        return 1;
+    }
+
+    public void printBfs() {
+
+        HuffmanNode node = this;
+        ArrayDeque<HuffmanNode> queue = new ArrayDeque<>();
+        queue.offer(node);
+
+        while(!queue.isEmpty()){
+            node = queue.poll();
+            System.out.println(node);
+            if (node.getLeft() != null) queue.offer(node.getLeft());
+            if (node.getRight() != null) queue.offer(node.getRight());
+        }
+    }
+}
+
+class HuffmanLeaf extends HuffmanNode {
+
+    private char letter;
+
+    public HuffmanLeaf(char letter, long freq) {
+        super(freq);
+        this.letter = letter;
+    }
+    public char getLetter() {
+        return letter;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Leaf freq: %d, char: %c", this.getFreq(), this.getLetter());
+    }
+}
